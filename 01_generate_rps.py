daf3323f147f823f248ef6f2bee3df934977de8c
Jennifer Gottschalk <jgottschalk@masseyhigh.school.nz>
2020-04-16 12:14:00 +1200

    Create 01_generate_rps.py

---
+++ 01_generate_rps.py
@@ -0,0 +1,9 @@
+import random
+
+# List to hold game items
+game_items = ["rock", "paper", "scissors"]
+
+# Choose an item from the list at random
+comp_choice = random.choice(game_items)
+
+print(comp_choice)
