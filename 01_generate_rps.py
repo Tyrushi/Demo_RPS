d81095ba787306ea7943ba50d37441b7c35cad33
Jennifer Gottschalk <jgottschalk@masseyhigh.school.nz>
2020-04-16 12:16:39 +1200

    Looped code (generate multiple items)
    Checks that code is randomly generating chioce from list.

--- 01_generate_rps.py
+++ 01_generate_rps.py
@@ -3,7 +3,8 @@ import random
 # List to hold game items
 game_items = ["rock", "paper", "scissors"]
 
-# Choose an item from the list at random
-comp_choice = random.choice(game_items)
+for item in range(1,20):
+    # Choose an item from the list at random
+    comp_choice = random.choice(game_items)
 
-print(comp_choice)
+    print(comp_choice)
