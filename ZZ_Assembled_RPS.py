5cd25cb9cdd85f0756a3abf73a612e979e5190c1
Jennifer Gottschalk <jgottschalk@masseyhigh.school.nz>
2020-06-03 20:12:42 +0000

    Added comparison function

--- ZZ_Assembled_RPS.py
+++ ZZ_Assembled_RPS.py
@@ -20,6 +20,33 @@ def string_checker(question, to_check):
     # if item not in list, print error
     print("sorry that is not a valid response")
 
+
+# Compare user choice and computer choice
+def rps_compare(rps_list, user, comp):
+
+  # Compare options...
+
+  # If they're the same, it's a tie
+  if user == comp:
+    result = "It's a tie!"
+
+  # Three ways to win
+  elif user == "rock" and comp == "scissors":
+    result = "You won!"
+  elif user == "paper" and comp == "rock":
+    result = "You won!"
+  elif user == "scissors" and comp == "paper":
+    result = "You won!"
+
+  # If one does not win / tie, it's a loss
+  else:
+    result = "You lost"
+
+  return result
+
+
+
+
 #  *** Main Routine starts here ***
 
 # Set up list for user choice AND computer choice
@@ -32,6 +59,10 @@ print(user_choice)
 comp_choice = random.choice(rps)
 print(comp_choice)
 
+compare = rps_compare(rps, user_choice, comp_choice)
+print(compare)
+
+# Compare user and computer choice
 
 
 
